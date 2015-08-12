198810beaa59d31e58d371a456d9fc77c72987be
Paul Nordin <codeprospects@gmail.com>
2015-08-12 13:24:13 -0700

    Refactor for readability and conciseness

--- trimeol.py
+++ trimeol.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Remove all end of line whitespace and tabs in a text file.
+# Remove all end of line whitespace and tabs in a text file(s).
 #
 # Usage:
 #   trimeol.py [input_files]
@@ -31,9 +31,9 @@
 #        Windows:
 #            dir /B /S /A:-D | findstr ".*\.c$" | trimeol.py
 
-import sys
 import os
 import stat
+import sys
 import time
 
 import binarycheck
@@ -44,27 +44,25 @@ def process_file(name):
     Returns 0 on success.
     """
     fname = name.rstrip(' \t\n\r')
-    indata = ""
     data = ""
-    start_size = 0
-    end_size = 0
+    original_len = 0
 
-    if (binarycheck.is_binary_file(fname)):
+    if binarycheck.is_binary_file(fname):
         print("Skipping {} - it appears to be a binary file.".format(fname))
         return -1
 
     try:
         with open(fname, 'r') as fin:
             for line in fin:
-                start_size += len(line)
+                original_len += len(line)
                 data = "".join([data,
                                 line.rstrip(' \t\n'),
                                 '\n'])
-        end_size = len(data)
-        if start_size == end_size: # No change in data
-            print("Skipping {} - no changes made.".format(fname))
+
+        if original_len == len(data): # No change in data
+            print("Skipping {} - no changes to be made.".format(fname))
             return -1
-            
+
         return write_data(data, fname)
     except IOError:
         print("An error occurred processing {}".format(fname))
@@ -84,36 +82,37 @@ def write_data(data, destination):
             os.fsync(fout.fileno())
         os.remove(destination)
         os.rename(tmpfile, destination)
-    except IOError, OSError:
+    except (IOError, OSError):
         print("An error occured writing {}".format(destination))
         return -1
 
     return 0
 
-def print_usage():
+def usage():
+    """Print usage information."""
     print("Usage: {} [input_files]\n"
-          "If no input_files are supplied,"
-          "the program reads from stdin.".format(os.path.basename(sys.argv[0])))
+          "If no input_files are supplied, the program reads "
+          "from stdin.".format(os.path.basename(sys.argv[0])))
 
 def main():
-    t1 = time.clock()
+    time1 = time.clock()
     nprocessed = 0
 
-    if (len(sys.argv) == 1):
+    if len(sys.argv) == 1:
         if stat.S_ISFIFO(os.fstat(0).st_mode): # Check for piped stdin.
             for line in sys.stdin:
-                if process_file(line.rstrip(' \t\n\r')) == 0:
+                if process_file(line.rstrip(" \t\n\r")) == 0:
                     nprocessed += 1
         else: # No input - print usage.
-            print_usage()
+            usage()
             sys.exit(0)
     else:
         for file in sys.argv[1:]:
             if process_file(file) == 0:
                 nprocessed += 1
 
-    t2 = time.clock()
-    print("{} files processed successfully in {} seconds.".format(nprocessed, round(t2 - t1, 4)))
+    time2 = time.clock()
+    print("{} files processed successfully in {} seconds.".format(nprocessed, round(time2 - time1, 4)))
 
-if __name__== "__main__":
-    main();
+if __name__ == "__main__":
+    main()
