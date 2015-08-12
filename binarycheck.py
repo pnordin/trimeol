71b4263d2a6097f68381638bc84ecd810f466cdb
Paul Nordin <codeprospects@gmail.com>
2015-08-12 13:46:50 -0700

    Remove binary file detection using the file command

--- binarycheck.py
+++ binarycheck.py
@@ -1,26 +1,7 @@
 
 import subprocess
 
-g_use_file_command = True
-
 def is_binary_file(fname):
-    """Determine if 'fname' is a binary file.
-
-    If the file command is available, it will be used,
-    otherwise, a heuristic approach is taken.
-
-    Returns True if 'fname' appears to be a binary file.
-    """
-    if (g_use_file_command and
-            is_binary_file_f(fname)):
-        return True
-
-    if is_binary_file_h(fname):
-        return True
-
-    return False
-
-def is_binary_file_h(fname):
     """Attempt to guess if 'fname' is a binary file heuristically.
 
     The algorithm has many flaws. Use with caution.
@@ -43,25 +24,6 @@ def is_binary_file_h(fname):
         ncontrol = control_char_count(chunk)
         ntext = len(chunk) - ncontrol
         return ncontrol > ntext
-
-def is_binary_file_f(fname):
-    """Determine if 'fname' is a binary file using the file command.
-
-    If the file command does not exist,
-    the global variable g_use_file_command is set to False
-    and no more attempts to use the file command should be made.
-
-    Returns True if'fname' appears to be a binary file.
-    """
-    try:
-        proc = subprocess.Popen(["file", fname], stdout=subprocess.PIPE)
-        for line in proc.stdout.readlines():
-            if "text" in line.lower():
-                return False
-    except:
-        g_use_file_command = False
-        return False
-    return True
 
 def is_control_char(c):
     """Return True if 'c' is a control character.
