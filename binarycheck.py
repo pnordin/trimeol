de8ff107422ec48f9241857ad85f01acc1841f50
Paul Nordin <codeprospects@gmail.com>
2015-08-12 12:32:40 -0700

    Update binary file check to use the file command if available

--- binarycheck.py
+++ binarycheck.py
@@ -1,4 +1,8 @@
 
+import subprocess
+
+g_use_file_command = True
+
 def is_control_char(c):
     """Return True if 'c' is a control character.
 
@@ -19,15 +23,16 @@ def control_char_count(data):
             n += 1
     return n
 
-def is_binary_file(fname):
-    """Return True if the file 'fname' is binary.
+def is_binary_file_h(fname):
+    """Attempt to guess if 'fname' is a binary file heuristically.
 
-    This only attempts to guess if the file is binary.
     The algorithm has many flaws. Use with caution.
-    It assumes that if a file has NUL bytes
+    It assumes that if a part of the file has NUL bytes
     or has more control characters than text characters,
     it is probably a binary file.
     An ASCII compatible character set is assumed.
+
+    Returns True if 'fname' appears to be a binary file.
     """
     with open(fname, 'rb') as fh:
         chunk = fh.read(1024)
@@ -41,3 +46,39 @@ def is_binary_file(fname):
         ncontrol = control_char_count(chunk)
         ntext = len(chunk) - ncontrol
         return ncontrol > ntext
+
+def is_binary_file_f(fname):
+    """Determine if 'fname' is a binary file using the file command.
+
+    If the file command does not exist,
+    the global variable g_use_file_command is set to False
+    and no more attempts to use the file command should be made.
+
+    Returns True if'fname' appears to be a binary file.
+    """
+    try:
+        proc = subprocess.Popen(["file", fname], stdout=subprocess.PIPE)
+        for line in proc.stdout.readlines():
+            if "text" in line.lower():
+                return False
+    except:
+        g_use_file_command = False
+        return False
+    return True
+
+def is_binary_file(fname):
+    """Determine if 'fname' is a binary file.
+
+    If the file command is available, it will be used,
+    otherwise, a heuristic approach is taken.
+
+    Returns True if 'fname' appears to be a binary file.
+    """
+    if (g_use_file_command and
+            is_binary_file_f(fname)):
+        return True
+
+    if is_binary_file_h(fname):
+        return True
+
+    return False
