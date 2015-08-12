8da7277ef0986e39a5c2a0eb0f7343bb299132c0
Paul Nordin <codeprospects@gmail.com>
2015-08-12 01:01:13 -0700

    Initial commit

---
+++ binarycheck.py
@@ -0,0 +1,43 @@
+
+def is_control_char(c):
+    """Return True if 'c' is a control character.
+
+    c is considered a control character if
+    it is outside of the extended ASCII set or
+    is a control character below 33.
+    An ASCII compatible character set is assumed.
+    """
+    charcode = ord(c)
+    return (charcode < 33 or
+            charcode > 255)
+
+def control_char_count(data):
+    """Return the count of control characters in 'data'."""
+    n = 0
+    for c in data:
+        if is_control_char(c):
+            n += 1
+    return n
+
+def is_binary_file(fname):
+    """Return True if the file 'fname' is binary.
+
+    This only attempts to guess if the file is binary.
+    The algorithm has many flaws. Use with caution.
+    It assumes that if a file has NUL bytes
+    or has more control characters than text characters,
+    it is probably a binary file.
+    An ASCII compatible character set is assumed.
+    """
+    with open(fname, 'rb') as fh:
+        chunk = fh.read(1024)
+
+        if not chunk: # Empty file
+            return False
+
+        if b'\x00' in chunk: # Has NUL bytes
+            return True
+
+        ncontrol = control_char_count(chunk)
+        ntext = len(chunk) - ncontrol
+        return ncontrol > ntext
---
+++ trimeol.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+
+# Remove all end of line whitespace and tabs in a text file.
+#
+# Usage:
+#   trimeol.py [input_files]
+#   If no input_files are supplied, the program reads from stdin.
+#
+# Examples:
+#    Perform on a single file:
+#        trimeol.py ~/project/main.c
+#
+#    Performs on multiple files:
+#        trimeol.py main.c builtins.h
+#
+#    Perform on all files in the current directory:
+#        *nix:
+#            find . -type f -maxdepth 1 | trimeol.py
+#        Windows:
+#            dir /B /A:-D | trimeol.py
+#
+#    Perform on all files in the current directory recursively:
+#        *nix:
+#            find . -type f | trimeol.py
+#        Windows:
+#            dir /B /S /A:-D | trimeol.py
+#
+#    Perform on all .c files in current directory recursively:
+#        *nix:
+#            find . -type f | grep ".*\.c$" | trimeol.py
+#        Windows:
+#            dir /B /S /A:-D | findstr ".*\.c$" | trimeol.py
+
+import sys
+import os
+import stat
+import time
+
+import binarycheck
+
+def process_file(name):
+    """Remove all end of line whitespace and tabs from the text file 'name'.
+
+    Returns 0 on success.
+    """
+    fname = name.rstrip(' \t\n\r')
+    data = ""
+
+    if (binarycheck.is_binary_file(fname)):
+        print("Skipping {} - it appears to be a binary file.".format(fname))
+        return -1
+
+    try:
+        with open(fname, 'r') as fin:
+            for line in fin:
+                data = "".join([data,
+                                line.rstrip(' \t\n\r'),
+                                '\n'])
+        return write_data(data, fname)
+    except IOError:
+        print("An error occurred processing {}".format(fname))
+        return -1
+
+def write_data(data, destination):
+    """Write (overwrite) 'data' to 'destination' file semi-atomically.
+
+    Returns 0 on success.
+    """
+    tmpfile = destination + ".tmp"
+
+    try:
+        with open(tmpfile, 'w') as fout:
+            fout.write(data)
+            fout.flush()
+            os.fsync(fout.fileno())
+        os.remove(destination)
+        os.rename(tmpfile, destination)
+    except IOError, OSError:
+        print("An error occured writing {}".format(destination))
+        return -1
+
+    return 0
+
+def print_usage():
+    print("Usage: {} [input_files]\n"
+          "If no input_files are supplied,"
+          "the program reads from stdin.".format(os.path.basename(sys.argv[0])))
+
+def main():
+    t1 = time.clock()
+    nprocessed = 0
+
+    if (len(sys.argv) == 1):
+        if stat.S_ISFIFO(os.fstat(0).st_mode): # Check for piped stdin.
+            for line in sys.stdin:
+                if process_file(line.rstrip(' \t\n\r')) == 0:
+                    nprocessed += 1
+        else: # No input - print usage.
+            print_usage()
+            sys.exit(0)
+    else:
+        for file in sys.argv[1:]:
+            if process_file(file) == 0:
+                nprocessed += 1
+
+    t2 = time.clock()
+    print("{} files processed successfully in {} seconds.".format(nprocessed, round(t2 - t1, 4)))
+
+if __name__== "__main__":
+    main();
