23b1c305179146a970b6e3aec68fb1ed76c04d08
Paul Nordin <codeprospects@gmail.com>
2015-08-12 18:15:47 -0700

    Add summary to output when processing completes

--- trimeol.py
+++ trimeol.py
@@ -12,18 +12,21 @@ import time
 
 import binarycheck
 
-def process_file(name):
+def process_file(fname):
     """Remove all end of line whitespace and tabs from the text file 'name'.
 
-    Returns 0 on success.
+    Returns:
+        -1 if error.
+        0 if file changed.
+        1 if file skipped.
+        2 if file not changed.
     """
-    fname = name.rstrip(" \t\n\r")    
     lines = []
     original_len = 0
 
     if binarycheck.is_binary_file(fname):
         print("Skipping {} - it appears to be a binary file.".format(fname))
-        return -1
+        return 1
 
     try:
         with open(fname, 'r') as fin:
@@ -32,13 +35,12 @@ def process_file(name):
                 lines.append(line.rstrip(" \t\n") + "\n")
         data = "".join(lines)
         if original_len == len(data): # No changes made
-            return -1
+            return 2
 
         return write_data(data, fname)
     except IOError:
         print("An error occurred processing {}".format(fname))
-
-    return -1
+        return -1
 
 def write_data(data, destination):
     """Write (overwrite) 'data' to 'destination' file semi-atomically.
@@ -48,7 +50,7 @@ def write_data(data, destination):
     tmpfile = destination + ".tmp"
 
     try:
-        with open(tmpfile, 'w') as fout:
+        with open(tmpfile, "w") as fout:
             fout.write(data)
             fout.flush()
             os.fsync(fout.fileno())
@@ -72,24 +74,40 @@ def usage():
 
 def main():
     time1 = time.clock()
-    nprocessed = 0
+    counters = {"processed": 0,
+                "skipped": 0,
+                "changed": 0,
+                "errors": 0}
+    files = []
 
     if len(sys.argv) == 1:
         if stat.S_ISFIFO(os.fstat(0).st_mode): # Check for piped stdin.
-            for line in sys.stdin:
-                if process_file(line.rstrip(" \t\n\r")) == 0:
-                    nprocessed += 1
+            files = sys.stdin
         else: # No input - print usage.
             usage()
             sys.exit(0)
     else:
-        for file in sys.argv[1:]:
-            if process_file(file) == 0:
-                nprocessed += 1
+        files = sys.argv[1:]
+
+    for file in files:
+        res = process_file(file.strip(" \r\n\r"))
+        if res >= 0:
+            counters["processed"] += 1
+            if res == 0:
+                counters["changed"] += 1
+            elif res == 1:
+                counters["skipped"] += 1
+        else:
+            counters["errors"] += 1
+
 
     time2 = time.clock()
-    print("{} files processed successfully in {} seconds.".format(nprocessed,
-                                                                  round(time2 - time1, 4)))
+    print("Finished processing after {} seconds.".format(round(time2 - time1, 4)))
+
+    col_width = max(len(row) for row in counters.keys()) + 2
+    print("=== Summary ===")
+    for key, value in counters.iteritems():
+        print("{}: {}".format(key.capitalize().ljust(col_width), value))
 
 if __name__ == "__main__":
     main()
